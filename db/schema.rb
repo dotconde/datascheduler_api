bdedfa7e618be4aa6b12b3e050d1d1763be80ac2
dotconde <iamdeyvi@gmail.com>
2024-05-06 14:45:13 -0500

    Add base schema for data_scheduler_api

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_04_173915) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_06_181741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.bigint "profile_id", null: false
+    t.string "title"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "location"
+    t.index ["profile_id"], name: "index_events_on_profile_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "company_id", null: false
+    t.integer "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_profiles_on_company_id"
+    t.index ["user_id", "company_id"], name: "index_profiles_on_user_id_and_company_id", unique: true
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
@@ -29,4 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_04_173915) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "profiles"
+  add_foreign_key "profiles", "companies"
+  add_foreign_key "profiles", "users"
 end
