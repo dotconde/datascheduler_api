63214106a4c04608bb7381146e0b694a61794221
dotconde <iamdeyvi@gmail.com>
2024-05-06 15:30:57 -0500

    Create default company and profile for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,25 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
+  after_create :create_default_company_and_profile
+
+  has_one :profile
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  def create_default_company_and_profile
+    return if self.profile.present?
+
+    default_company = Company.create(name: 'My company')
+
+    Profile.create(user: self, company: default_company, role: :admin)
+  end
+
+  def switch_company_and_role(new_company_id, new_role)
+    self.profile.update!(role: new_role, company_id: new_company_id)
+  end
 end
