63471289c392bea76cef1a74923454b9f8150da7
dotconde <iamdeyvi@gmail.com>
2024-05-06 15:18:47 -0500

    Set json as default format for app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_default_response_format
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name])
+  end
+
+  private
+
+  def set_default_response_format
+    request.format = :json
   end
 end
