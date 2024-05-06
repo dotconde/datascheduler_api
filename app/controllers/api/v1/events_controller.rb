103ee326c8ebfec0d95961392ae11222c4d9d444
dotconde <iamdeyvi@gmail.com>
2024-05-06 15:46:24 -0500

    Add event's view per range

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,12 +2,28 @@ module Api
   module V1
     class EventsController < ApplicationController
       before_action :authenticate_user!
+      before_action :set_profile, only: %i[view]
 
       def index
         today_events = Event.today_events(current_user)
 
         render json: { events: today_events },
                status: :ok
+      end
+
+      def view
+        start_date = params[:event][:from_date]
+        end_date = params[:event][:to_date]
+
+        if current_user.can_view_events_of?(@profile)
+          events = Event.events_within_date_range(@profile, start_date, end_date)
+
+          render json: { events: events },
+                 status: :ok
+        else
+          render json: { error: 'You are not authorized to perform this action' },
+                 status: :forbidden
+        end
       end
     end
   end
